b3941c7db7c45c6931336877e5c1c1df22c137f1
mostri28 <mateoetchepare@gmail.com>
2022-08-30 15:08:00 -0300

    creacion archivo
    .-

---
+++ TP1PrimeraParte.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+
+void leeArch(char cadena[], int *N);
+
+int main() {
+
+    leeArch()
+    return 0;
+}
+
+void leeArch(char cadena[], int *N) {
+    int i;
+    FILE* archt; // archivo de texto
+
+}
