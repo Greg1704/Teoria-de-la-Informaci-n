68bd57ca38e985502e4980601ed373e8e7ff30bb
mostri28 <mateoetchepare@gmail.com>
2022-08-30 15:37:36 -0300

    haciendo inciso a

--- TP1PrimeraParte.c
+++ TP1PrimeraParte.c
@@ -2,31 +2,51 @@
 #include <stdlib.h>
 #include "string.h"
 
-void leeArch(char cadena[]); // parámetro N por si llega a haber una letra de más o de menos
+void leeArch(int M[3][3]); // parámetro N por si llega a haber una letra de más o de menos
+void init(int M[][], int V[]);
+void calculaProbabilidades(int MPasaje[][]);
 
 int main() {
-
-    leeArch()
+    int M[3][3];
+    leeArch(M);
     return 0;
 }
 
-void leeArch(char cadena[]) {
+void init(int M[3][3], int V[3]) { // M va a guardar en cada mij la ocurrencia de i despues de la ocurrencia de j
+    int i, j;
+    for (i=0 ; i<3 ; i++) {
+        V[i]=0;
+        for (j=0 ; j<3 ; j++) {
+            M[i][j]=0;
+        }
+    }
+}
+
+void leeArch(int M[3][3]) {
     int i=0, ocA=0, ocB=0, ocC=0; // si fuesen muchos simb haria un vector de ocurrencias
-    char simb;
+    int MPasaje[3][3]
+    char simb, ultSimb;
     FILE* archt; // archivo de texto
+
+    initMatriz(M);
     archt = fopen("datos.txt", "r");
     fscanf(archt, "%c", &simb)
     while (!feof(archt)) {
-        if (simb == 'a') {
-            ocA+=1;
-        } else
-            if (simb == 'b')
-                ocB+=1;
-            else
-                if (simb == 'c') // verifico tambien el caso de C por si llega a haber algun simbolo que no corresponda
-                    ocC+=1;
-        cadena[i] = simb;
+        V[simb-97]++; // hago un vector para ir guardando las ocurrencias
+        M[simb][ultSimb]+=1; // el simbolo "simb" aparecio despues de la ocurrencia de "ultSimb"
+        //cadena[i] = simb;
+        ultSimb=simb;
         fscanf(archt, "%c", &simb);
-        i++;
+        i++
+    }
+    calculaProbabilidades(MPasaje, M, V)
+}
+
+void calculaProbabilidades(int MPasaje[3][3], int M[3][3], int V[]) {
+    int i, j;
+    for (j=0 ; j<3 ; i++) {
+        for (i=0 ; i<3 ; j++) {
+            MPasaje[i][j] = (M[i][j] / V[i]);
+        }
     }
 }
