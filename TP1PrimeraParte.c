21501f225a650b6d39588b12e61309a161df739b
mostri28 <mateoetchepare@gmail.com>
2022-08-30 15:09:29 -0300

    xd

--- TP1PrimeraParte.c
+++ TP1PrimeraParte.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "string.h"
 
-void leeArch(char cadena[], int *N);
+void leeArch(char cadena[]); // parámetro N por si llega a haber una letra de más o de menos
 
 int main() {
 
@@ -10,8 +10,23 @@ int main() {
     return 0;
 }
 
-void leeArch(char cadena[], int *N) {
-    int i;
+void leeArch(char cadena[]) {
+    int i=0, ocA=0, ocB=0, ocC=0; // si fuesen muchos simb haria un vector de ocurrencias
+    char simb;
     FILE* archt; // archivo de texto
-
+    archt = fopen("datos.txt", "r");
+    fscanf(archt, "%c", &simb)
+    while (!feof(archt)) {
+        if (simb == 'a') {
+            ocA+=1;
+        } else
+            if (simb == 'b')
+                ocB+=1;
+            else
+                if (simb == 'c') // verifico tambien el caso de C por si llega a haber algun simbolo que no corresponda
+                    ocC+=1;
+        cadena[i] = simb;
+        fscanf(archt, "%c", &simb);
+        i++;
+    }
 }
