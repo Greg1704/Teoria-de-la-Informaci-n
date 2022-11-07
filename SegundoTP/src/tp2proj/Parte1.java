844201fbcfa2264da022b9d3c6ec34b8a4028530
Franco <fraansardi@gmail.com>
2022-11-07 12:37:52 -0300

    Projecto de JDev

---
+++ SegundoTP/src/tp2proj/Parte1.java
@@ -0,0 +1,11 @@
+package tp2proj;
+
+public class Parte1 {
+    public Parte1() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Parte1 parte1 = new Parte1();
+    }
+}
