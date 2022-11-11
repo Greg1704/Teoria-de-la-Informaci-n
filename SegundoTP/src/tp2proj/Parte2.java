3d599086d37ecb30a786da352348d62b781a0e80
Mateo Etchepare <81594833+mateoetchepare@users.noreply.github.com>
2022-11-11 12:04:49 -0300

    Paso de mi PC al Git. Todo hecho.
    Varios calculos erroneos, hay que revisar

--- SegundoTP/src/tp2proj/Parte2.java
+++ SegundoTP/src/tp2proj/Parte2.java
@@ -1,4 +1,4 @@
-package tp2proj;
+
 
 public class Parte2 {
     public static final int NumGrupo = 11;
@@ -14,18 +14,106 @@ public class Parte2 {
     public static void main(String[] args) {
 
         int i = 0;
-        double V1[] = {0.2, 0.1, 0.3, 0.3, 0.1};
-        double V2[] = {0.25, 0.33, 0.27, 0.15};
-        double V3[] = {0.15, 0.1, 0.20, 0.25, 0.14, 0.16};
+        double VProb1[] = {0.2, 0.1, 0.3, 0.3, 0.1};
+        double VProb2[] = {0.25, 0.33, 0.27, 0.15};
+        double VProb3[] = {0.15, 0.1, 0.20, 0.25, 0.14, 0.16};
 
         double MC1[][] = new double[N1][M1];
         double MC2[][] = new double[N2][M2];
         double MC3[][] = new double[N3][M3];
 
-        cargaMatrices(MC1, MC2, MC3);
-        System.out.println("La entropia 'a priori' de la entrada" + 1 + "es de " + entropiaAPriori(V1));
-        System.out.println("La entropia 'a priori' de la entrada" + 2 + "es de " + entropiaAPriori(V2));
-        System.out.println("La entropia 'a priori' de la entrada" + 3 + "es de " + entropiaAPriori(V3));
+        double VProbSalida1[] = new double[M1];
+        double VProbSalida2[] = new double[M2];
+        double VProbSalida3[] = new double[M3];
+
+        double MCInversa1[][] = new double[M1][N1];
+        double MCInversa2[][] = new double[M2][N2];
+        double MCInversa3[][] = new double[M3][N3];
+
+        cargaMatrices(MC1, MC2, MC3); // CARGO TODAS LAS MATRICES DE CANAL
+
+        calculaProbSalidas(MC1, VProb1, VProbSalida1, N1, M1); // CARGO LAS PROBABILIDADES DE SALIDA
+        calculaProbSalidas(MC2, VProb2, VProbSalida2, N2, M2);
+        calculaProbSalidas(MC3, VProb3, VProbSalida3, N3, M3);
+
+        matrizCanalInversa(MCInversa1, MC1, VProb1, VProbSalida1); // EMPIEZO A CARGAR LAS MATRICES Ai/Bj
+        matrizCanalInversa(MCInversa2, MC2, VProb2, VProbSalida2);
+        matrizCanalInversa(MCInversa3, MC3, VProb3, VProbSalida3);
+
+        double[][] MSS1 = new double[N1][M1];
+        double[][] MSS2 = new double[N2][M2];
+        double[][] MSS3 = new double[N3][M3];
+
+        matrizSucesoSimultaneo(MSS1, MC1, VProb1, M1); // CARGO MATRICES (A,B)
+        matrizSucesoSimultaneo(MSS2, MC2, VProb2, M2);
+        matrizSucesoSimultaneo(MSS3, MC3, VProb3, M3);
+
+
+        System.out.println("CANAL 1");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("La entropia 'a priori' de la entrada " + " 1 " + "es de " + entropiaAPriori(VProb1));
+        System.out.println("La entropia de salida de " + " 1 " + " es de " + entropiaSalida(VProbSalida1));
+        System.out.println("La equivocacion para el canal " + " 1 " + " es de " + equivocacion(MC1, VProb1, VProbSalida1));
+        System.out.println("La informacion mutua para la matriz de canal " + " 1 " + " es " + informacionMutua(MC1, VProb1, VProbSalida1));
+        System.out.println("La equivocacion para la matriz de canal inversa " + " 1 " + " es " + equivocacion(MCInversa1, VProbSalida1, VProb1));
+        System.out.println("La informacion mutua para la matriz de canal inversa " + " 1 " + " es " + informacionMutua(MCInversa1, VProbSalida1, VProb1));
+        System.out.println("La entropia 'afin' para la matriz " + " 1 " + " es " + entropiaAFin(MSS1));
+        System.out.println("");
+
+
+
+
+
+
+
+
+        System.out.println("CANAL 2");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("La entropia 'a priori' de la entrada " + " 2 " + "es de " + entropiaAPriori(VProb2));
+        System.out.println("La entropia de salida de " + " 2 " + " es de " + entropiaSalida(VProbSalida2));
+        System.out.println("La equivocacion para el canal " + " 2 " + " es de " + equivocacion(MC2, VProb2, VProbSalida2));
+        System.out.println("La informacion mutua para la matriz de canal " + " 2 " + " es " + informacionMutua(MC2, VProb2, VProbSalida2));
+        System.out.println("La equivocacion para la matriz de canal inversa " + " 2 " + " es " + equivocacion(MCInversa2, VProbSalida2, VProb2));
+        System.out.println("La informacion mutua para la matriz de canal inversa " + " 2 " + " es " + informacionMutua(MCInversa2, VProbSalida2, VProb2));
+        System.out.println("La entropia 'afin' para la matriz " + " 2 " + " es " + entropiaAFin(MSS2));
+        System.out.println("");
+
+
+
+
+
+        System.out.println("CANAL 3");
+        System.out.println("---------------------------------------------------------------------");
+
+        System.out.println("La entropia 'a priori' de la entrada " + " 3 " + "es de " + entropiaAPriori(VProb3));
+        System.out.println("La entropia de salida de " + " 3 " + " es de " + entropiaSalida(VProbSalida3));
+        System.out.println("La equivocacion para el canal " + " 3 " + " es de " + equivocacion(MC3, VProb3, VProbSalida3));
+        System.out.println("La informacion mutua para la matriz de canal " + " 3 " + " es " + informacionMutua(MC3, VProb3, VProbSalida3));
+        System.out.println("La equivocacion para la matriz de canal inversa " + " 3 " + " es " + equivocacion(MCInversa3, VProbSalida3, VProb3));
+        System.out.println("La informacion mutua para la matriz de canal inversa " + " 3 " + " es " + informacionMutua(MCInversa3, VProbSalida3, VProb3));
+        System.out.println("La entropia 'afin' para la matriz " + " 3 " + " es " + entropiaAFin(MSS3));
+        System.out.println("");
+
+
+
+
+
+        System.out.println("MATRIZ DE CANAL 1");
+        System.out.println("------------------------------------------------");
+        System.out.println(MC1[0][0] + " " + MC1[0][1] + " " + MC1[0][2]);
+        System.out.println(MC1[1][0] + " " + MC1[1][1] + " " + MC1[1][2]);
+        System.out.println(MC1[2][0] + " " + MC1[2][1] + " " + MC1[2][2]);
+        System.out.println(MC1[3][0] + " " + MC1[3][1] + " " + MC1[3][2]);
+        System.out.println(MC1[4][0] + " " + MC1[4][1] + " " + MC1[4][2]);
+
+        System.out.println("MATRIZ DE CANAL INVERSA 1");
+        System.out.println("------------------------------------------------");
+        System.out.println(MCInversa1[0][0] + " " + MCInversa1[0][1] + " " + MCInversa1[0][2] + " "  + MCInversa1[0][3] + " " + MCInversa1[0][4]);
+        System.out.println(MCInversa1[1][0] + " " + MCInversa1[1][1] + " " + MCInversa1[1][2] + " "  + MCInversa1[1][3] + " " + MCInversa1[1][4]);
+        System.out.println(MCInversa1[2][0] + " " + MCInversa1[2][1] + " " + MCInversa1[2][2] + " "  + MCInversa1[2][3] + " " + MCInversa1[2][4]);
+
+
+
     }
 
     public static void cargaMatrices(double MC1[][], double MC2[][], double MC3[][]) {
@@ -35,7 +123,8 @@ public class Parte2 {
 
         MC1[1][0] = defineElemMC1('b');
         MC1[1][1] = 0.4;
-        MC1[1][2] = 1 - MC1[1][0] - MC1[1][1];;
+        MC1[1][2] = 1 - MC1[1][0] - MC1[1][1];
+        ;
 
         MC1[2][0] = 0.3;
         MC1[2][1] = defineElemMC1('a');
@@ -108,8 +197,8 @@ public class Parte2 {
         double aux = 0;
         if (c == 'a') {
             aux = 0.03 * NumGrupo;
-            if ((aux < 0.1) ||(aux > 0.3))
-            aux = 0.3;
+            if ((aux < 0.1) || (aux > 0.3))
+                aux = 0.3;
         } else if (c == 'b') {
             aux = 0.04 * NumGrupo;
             if (aux < 0.1 || aux > 0.4) {
@@ -123,19 +212,18 @@ public class Parte2 {
         double aux = 0;
         if (c == 'a') {
             aux = 0.03 * NumGrupo;
-            if ((aux < 0.1) ||(aux > 0.3))
+            if ((aux < 0.1) || (aux > 0.3))
                 aux = 0.3;
         } else if (c == 'b') {
             aux = 0.02 * NumGrupo;
             if (aux < 0.1 || aux > 0.2) {
                 aux = 0.2;
-            } else
-                if (c == 'c') {
-                    aux = 0.03 * NumGrupo;
-                    if (aux < 0.1 || aux > 0.3) {
-                        aux = 0.3;
-                    }
+            } else if (c == 'c') {
+                aux = 0.03 * NumGrupo;
+                if (aux < 0.1 || aux > 0.3) {
+                    aux = 0.3;
                 }
+            }
         }
         return aux;
     }
@@ -144,14 +232,13 @@ public class Parte2 {
         double aux = 0;
         if (c == 'a') {
             aux = 0.03 * NumGrupo;
-            if ((aux < 0.1) ||(aux > 0.3))
+            if ((aux < 0.1) || (aux > 0.3))
                 aux = 0.3;
         } else if (c == 'b') {
             aux = 0.02 * NumGrupo;
             if (aux < 0.1 || aux > 0.2) {
                 aux = 0.2;
-            } else
-            if (c == 'c') {
+            } else if (c == 'c') {
                 aux = 0.03 * NumGrupo;
                 if (aux < 0.1 || aux > 0.3) {
                     aux = 0.3;
@@ -164,15 +251,81 @@ public class Parte2 {
     public static double entropiaAPriori(double[] VProb) {
         double auxEntrop = 0;
         int i = 0;
-        for (i = 0 ; i < VProb.length ; i++)
+        for (i = 0; i < VProb.length; i++)
             auxEntrop += (VProb[i] * (-Math.log(VProb[i])) / Math.log(2));
         return auxEntrop;
     }
 
-    public static double equivocacion(double M[][]) {
-        double auxEquivoc = 0;
-        return auxEquivoc;
+    public static double entropiaSalida(double[] VProbSalida) {
+        double auxEntrop = 0;
+        int i = 0;
+        for (i = 0 ; i < VProbSalida.length ; i++) {
+            auxEntrop += VProbSalida[i] * (-Math.log(VProbSalida[i])) / Math.log(2);
+        }
+        return auxEntrop;
+    }
+
+    public static void calculaProbSalidas(double[][] MC, double[] VProb, double[] VProbSalida, int N, int M) {
+        int i;
+        for (i = 0 ; i < M ; i++) {
+            VProbSalida[i] = 0;
+            for (int j = 0 ; j < N ; j++) {
+                VProbSalida[i] += VProb[j] * MC[j][i];
+            }
+        }
+    }
+
+    public static void matrizCanalInversa(double MCInversa[][], double[][] MC, double[] VProb, double[] VProbSalida) {
+        int i = 0;
+        for(i = 0 ; i < VProbSalida.length ; i++){
+            for(int j = 0 ; j < VProb.length ; j++){
+                MCInversa[i][j] = (MC[j][i] * VProb[j]) / VProbSalida[i]; // P(Bj / Ai) . P(Ai) / P(Bj) // quedan las filas como las salidas y las columnas como entradas
+            }
+        }
+    }
+
+    public static double entropiaAPosteriori(double[][] MC, double[] VProb, double[] VProbSalida, int j) {
+        double aux = 0;
+        int i = 0;
+        for (i = 0 ; i < VProb.length-1 ; i++) {
+            double prob = (MC[i][j] * VProb[i]) / VProbSalida[j]; // (P(bj / ai) * P(ai)) / P(bj)
+            if (prob > 0) {
+                aux +=  prob * (-Math.log(prob) / Math.log(2)); // prob * -log(prob)
+            }
+        }
+        return aux;
+    }
+
+    public static double equivocacion(double[][] MC, double[] VProb, double[] VProbSalida) {
+        double auxEquivocacion = 0;
+        for(int j=0 ; j < VProbSalida.length ; j++) {
+            auxEquivocacion += entropiaAPosteriori(MC, VProb, VProbSalida, j) * VProbSalida[j];
+        }
+        return auxEquivocacion;
+    }
+
+    public static double informacionMutua(double[][] MC, double [] VProb, double[] VProbSalida) {
+        return entropiaAPriori(VProb) - equivocacion(MC, VProb, VProbSalida);
+    }
+
+    public static void matrizSucesoSimultaneo(double[][] MSS, double[][] MC, double[] VProb, int M) {
+        int i, j;
+        for (i = 0 ; i < VProb.length ; i++) {
+            for (j = 0 ; j < M ; j++) {
+                MSS[i][j] = MC[i][j] * VProb[i];
+            }
+        }
+    }
+
+    public static double entropiaAFin(double[][] MSS) {
+        double auxEntrop = 0;
+        for (double[] VProb : MSS) {
+            for (double prob: VProb){ // recorre toda la matriz
+                if (prob > 0)
+                    auxEntrop += prob * (-Math.log(prob) / Math.log(2));
+            }
+        }
+        return auxEntrop;
     }
 
 }
-
